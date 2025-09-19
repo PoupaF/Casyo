5db137b2af1523f1c88d677aa56275000e587ca2
PoupaF <jmvivasszz@gmail.com>
2025-09-19 01:52:41 -0300

    Se agregan en main, Funciones: menuPrincipal, menuFiguras_2d, main, y se agregan los imports de figuras_2d y utilidades; en utilidades se agrega la funcion: validacionEntradaUsuario; Se debugea y se realizan pruebas

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-"print('�Calculadora Geom�trica iniciada!')" 
+from figuras_2d import * #importa codigo que no tenemos en el main
+from utilidades import *
+
+def menuPrincipal():
+    #Menu Principal
+    print("\n--- Calculadora Geométrica---")
+    print("Seleccione un valor númerico para comenzar:")
+    print(" 01-Figuras 2D")
+    print(" 99-Salir")
+
+def menuFiguras_2d():
+    #Menu de las figuras bidimensionales
+    print("\n---Figuras 2D---")
+    print("Seleccione un valor para continuar:")
+    print(" 01-Cuadrado")
+    print(" 02-Rectangulo")
+    print(" 03-Circulo")
+    print(" 04-Triangulo")
+
+def main():
+    #Funcion principal en bicle
+    while True:
+        menuPrincipal() #Mostrar el menu principal
+        EntradaNumericaUsuario = validacionEntradaUsuario("elige un número: ")
+        
+        if EntradaNumericaUsuario == 1:
+            #ejecutar código de figuras bidimensionales
+            print("ejecutando bidimensionales")
+
+        elif EntradaNumericaUsuario == 99:
+            print("bays")
+            break
+
+if __name__ == "__main__":
+    main()
--- utilidades.py
+++ utilidades.py
@@ -1,1 +1,8 @@
-"# Funciones auxiliares" 
+def validacionEntradaUsuario(mensaje):
+    #Validar que el usuario ingrese un maldito numero
+    while True:
+        try:
+            valor = float(input(mensaje))
+            return valor
+        except ValueError:
+            print("Error, ingrese un número válido")
